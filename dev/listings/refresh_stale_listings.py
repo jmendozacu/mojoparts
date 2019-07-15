f9dfb306d0e66b6b4b1e5801b3c57f572bff65a8
Ryan Hand <ryanwhand@gmail.com>
2019-07-15 08:01:16 -0400

    initial commit to start project for automatically refrehsing stale ebay listings

---
+++ dev/listings/refresh_stale_listings.py
@@ -0,0 +1,7 @@
+#download a report of active ebay listings for diy where item is in price file
+
+#if the listing is ending tomorrow and has qty available and has not sold
+
+#	add the eBay item ID to end listing csv
+
+#	if it is a non-catalog listing (PFG), then add it to the bulk upload file to create new listings

decbdac70a8ee4ea4f6b0434b742d6b1fcdd2d74
Ryan Hand <ryanwhand@gmail.com>
2019-07-17 12:10:30 +0000

    remove shipping code replaced by channel spyder

--- shipping/shipping_brocksupply.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/class.shipping_import.php';
-if(strpos(dirname(__FILE__),'public_html')===false){
-	$isDebug = false;
-}else{
-	$isDebug = true;
-}
-$si = new shipping_import($isDebug);
-$si->brocksupply_run();

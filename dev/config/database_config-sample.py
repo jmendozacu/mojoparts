9a0d2e582719bf56223a70056b69a1149a1b59e7
Ryan Hand <ryanwhand@gmail.com>
2019-07-17 11:22:04 -0400

    add sample db config file for python apps

---
+++ dev/config/database_config-sample.py
@@ -0,0 +1,5 @@
+## RENAME THIS TO database_config.py
+mysql = {'host': '## INSERT HOSTNAME HERE ##',
+         'user': '## INSERT USER HERE ##',
+         'passwd': '## INSERT PASSWORD HERE ##',
+         'db': '## INSERT DATABSE NAME HERE ##'}

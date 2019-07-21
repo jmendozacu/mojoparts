e4f8c3ad798861e352877fbf37824b6bca0afd9a
Ryan Hand <ryanwhand@gmail.com>
2019-07-21 07:57:12 -0400

    add the ability to create the string of image urls used for the bulk upload

--- dev/ListingAutomation/refresh_stale_listings.py
+++ dev/ListingAutomation/refresh_stale_listings.py
@@ -1,3 +1,6 @@
+# TODO: get_image_string needs to be changed so that position 0 is first
+#       and that only 5 images are added
+
 import database_config as config
 import pymysql
 import csv
@@ -5,40 +8,66 @@ import os
 from datetime import datetime
 from datetime import timedelta
 
+
+def get_image_string(mage_sku_, cursor):
+    image_query = " \
+            SELECT mg.value \
+            FROM catalog_product_entity cpe \
+            left join catalog_product_entity_media_gallery mg ON mg.entity_id=cpe.entity_id AND mg.attribute_id=88 \
+            WHERE cpe.sku= %s \
+    ;"
+    img_string = ""
+    img_url_prefix = "http://mojoparts.com/media/catalog/product"
+    cursor.execute(image_query, (mage_sku,))
+    for img_record in cursor.fetchall():
+        if img_string != "":
+            img_string = img_string + "||"
+        img_string = img_string + img_url_prefix + img_record[0]
+    return img_string
+
+
+# initialize variables
 today = datetime.date(datetime.today())
 tomorrow = datetime.date(datetime.today() + timedelta(days=1))
-connection = pymysql.connect(host=config.mysql["host"], user=config.mysql["user"], password=config.mysql["passwd"], db=config.mysql["db"])
-product_query = "SELECT cpe.sku, \
-    css.VALUE AS 'cs_sku', \
-    ttl.VALUE AS 'ebay_title', \
-    cat.VALUE AS 'ebay_category', \
-    bi.VALUE AS 'base_image', \
-    mpn.value as 'Manufacturer Part Number', \
-    hol.value as 'Interchange Part Number', \
-    plc.value as 'Placement on Vehicle', \
-    sfc.value as 'Surface Finish', \
-    plk.value as 'Partslink', \
-    oem.value as 'OEM Number', \
-    clr.value as 'Color', \
-    adn.value as 'additional notes' \
-    FROM catalog_product_entity cpe inner join catalog_product_entity_int sts on cpe.entity_id = sts.entity_id and sts.attribute_id=96 and sts.value=1\
-    inner JOIN catalog_product_entity_int v ON v.entity_id=cpe.entity_id AND v.attribute_id=163 AND v.VALUE=36  \
-    LEFT JOIN catalog_product_entity_varchar css ON css.entity_id=cpe.entity_id AND css.attribute_id=223 \
-    left JOIN catalog_product_entity_varchar ttl ON ttl.entity_id=cpe.entity_id AND ttl.attribute_id=153 \
-    left JOIN catalog_product_entity_varchar cat ON cat.entity_id=cpe.entity_id AND cat.attribute_id=147 \
-    left JOIN catalog_product_entity_varchar bi ON bi.entity_id=cpe.entity_id AND bi.attribute_id=85 \
-    left JOIN catalog_product_entity_varchar mpn ON mpn.entity_id=cpe.entity_id AND mpn.attribute_id=177 \
-    left JOIN catalog_product_entity_varchar hol ON hol.entity_id=cpe.entity_id AND hol.attribute_id=138 \
-    left JOIN catalog_product_entity_varchar plc ON plc.entity_id=cpe.entity_id AND plc.attribute_id=143 \
-    left JOIN catalog_product_entity_int sfc ON sfc.entity_id=cpe.entity_id AND sfc.attribute_id=155 \
-    left JOIN catalog_product_entity_varchar plk ON plk.entity_id=cpe.entity_id AND plk.attribute_id=142 \
-    left JOIN catalog_product_entity_varchar oem ON oem.entity_id=cpe.entity_id AND oem.attribute_id=140 \
-    left JOIN catalog_product_entity_int clr ON clr.entity_id=cpe.entity_id AND clr.attribute_id=92 \
-    left JOIN catalog_product_entity_text adn ON adn.entity_id=cpe.entity_id AND adn.attribute_id=168 \
-    WHERE css.value = %s \
-    ORDER BY cpe.sku \
-    ;"
-    
+db_connection = pymysql.connect(host=config.mysql["host"], user=config.mysql["user"], password=config.mysql["passwd"], db=config.mysql["db"])
+product_query = " \
+        SELECT cpe.sku, \
+        css.VALUE AS 'cs_sku', \
+        ttl.VALUE AS 'ebay_title', \
+        cat.VALUE AS 'ebay_category', \
+        bi.VALUE AS 'base_image', \
+        mpn.value as 'Manufacturer Part Number', \
+        hol.value as 'Interchange Part Number', \
+        plc.value as 'Placement on Vehicle', \
+        sfc.value as 'Surface Finish', \
+        plk.value as 'Partslink', \
+        oem.value as 'OEM Number', \
+        clr.value as 'Color', \
+        adn.value as 'additional notes', \
+        ecn.category_name \
+        FROM catalog_product_entity cpe inner join catalog_product_entity_int sts on cpe.entity_id = sts.entity_id and sts.attribute_id=96 and sts.value=1\
+        inner JOIN catalog_product_entity_int v ON v.entity_id=cpe.entity_id AND v.attribute_id=163 AND v.VALUE=36  \
+        LEFT JOIN catalog_product_entity_varchar css ON css.entity_id=cpe.entity_id AND css.attribute_id=223 \
+        left JOIN catalog_product_entity_varchar ttl ON ttl.entity_id=cpe.entity_id AND ttl.attribute_id=153 \
+        left JOIN catalog_product_entity_varchar cat ON cat.entity_id=cpe.entity_id AND cat.attribute_id=147 \
+        left JOIN catalog_product_entity_varchar bi ON bi.entity_id=cpe.entity_id AND bi.attribute_id=85 \
+        left JOIN catalog_product_entity_varchar mpn ON mpn.entity_id=cpe.entity_id AND mpn.attribute_id=177 \
+        left JOIN catalog_product_entity_varchar hol ON hol.entity_id=cpe.entity_id AND hol.attribute_id=138 \
+        left JOIN catalog_product_entity_varchar plc ON plc.entity_id=cpe.entity_id AND plc.attribute_id=143 \
+        left JOIN catalog_product_entity_int sfc ON sfc.entity_id=cpe.entity_id AND sfc.attribute_id=155 \
+        left JOIN catalog_product_entity_varchar plk ON plk.entity_id=cpe.entity_id AND plk.attribute_id=142 \
+        left JOIN catalog_product_entity_varchar oem ON oem.entity_id=cpe.entity_id AND oem.attribute_id=140 \
+        left JOIN catalog_product_entity_int clr ON clr.entity_id=cpe.entity_id AND clr.attribute_id=92 \
+        left JOIN catalog_product_entity_text adn ON adn.entity_id=cpe.entity_id AND adn.attribute_id=168 \
+        left JOIN mojo_ebay_category_names ecn ON ecn.category_number=cat.VALUE \
+        WHERE css.value = %s \
+        ORDER BY cpe.sku \
+        ;"
+
+###############################################################################    
+# mainline
+###############################################################################    
+
 with open("active-listings.csv", "r") as input_file, \
     open(f"{today}-listings-to-end.csv", "w+", newline="") as end_listings_file, \
     open(f"{today}-diy-pfg-eBay.csv", "w+", newline="") as bulk_listings_file:
@@ -46,7 +75,7 @@ with open("active-listings.csv", "r") as input_file, \
     input_reader = csv.reader(input_file)
     end_writer = csv.writer(end_listings_file)
     bulk_writer = csv.writer(bulk_listings_file)
-    cursor = connection.cursor()
+    cursor = db_connection.cursor()
 
     next(input_reader)
     end_writer.writerow(["itemID", "cs-sku", "sold_qty", "stock_qty", "start_date", "end_date"])
@@ -55,6 +84,7 @@ with open("active-listings.csv", "r") as input_file, \
 
     for row in input_reader:
         cs_sku = row[4]
+        cs_partno = cs_sku[4:]
         listing_sku = row[5]
         sold_qty = int(row[9])
         stock_qty = int(row[16])
@@ -71,44 +101,47 @@ with open("active-listings.csv", "r") as input_file, \
 
             # for non-catalog listings, build a bulk upload to recreate listings
             if cs_sku[:3] == "366":
-                cursor.execute(product_query, (cs_sku,))
+                cursor.execute(product_query, (listing_sku))
                 record = cursor.fetchone()
                 if record != None:
+                    mage_sku = record[0]
                     ebay_title = record[2]
-                    epid = cs_sku[3:].replace("-", "").strip() 
-                    bulk_writer.writerow([cs_sku[:3], \
-                                          cs_sku[3:], \
-                                          "1", \
-                                          ebay_title, \
-                                          epid, \
-                                          "StoresFixedPrice", \
-                                          "46074", \
-                                          "GTC", \
-                                          row[15], \
-                                          row[16], \
-                                          record[4], \
-                                          "TODO: category name", \
-                                          "TODO: images", \
-                                          "TODO: item description", \
-                                          "1000||New", \
-                                          "2", \
-                                          "US", \
-                                          "", \
-                                          "US", \
-                                          "TODO: fulfillment policy id", \
-                                          "TODO: payment policy id", \
-                                          "TODO: return policy id", \
-                                          "Aftermarket Replacement", \
-                                          "1 Year", \
-                                          "Direct Replacement", \
-                                          "DOT/SAE", \
-                                          "TODO: placement on vehicle", \
-                                          "TODO: mpn", \
-                                          "TODO: interchange", \
-                                          "TODO: surface finish", \
-                                          "TODO: superseded part #", \
-                                          "TODO: partslink", \
-                                          "TODO: oem", \
-                                          "TODO: color", \
-                                          ])
-connection.close()
+                    epid = cs_partno.replace("-", "").strip() 
+                    img_string = get_image_string(mage_sku, cursor)
+                    bulk_writer.writerow(
+                            [cs_sku[:3], \
+                            cs_partno, \
+                            "1", \
+                            ebay_title, \
+                            epid, \
+                            "StoresFixedPrice", \
+                            "46074", \
+                            "GTC", \
+                            row[15], \
+                            row[16], \
+                            record[4], \
+                            record[13], \
+                            img_string, \
+                            "TODO: item description", \
+                            "1000||New", \
+                            "2", \
+                            "US", \
+                            "", \
+                            "US", \
+                            "TODO: fulfillment policy id", \
+                            "TODO: payment policy id", \
+                            "TODO: return policy id", \
+                            "Aftermarket Replacement", \
+                            "1 Year", \
+                            "Direct Replacement", \
+                            "DOT/SAE", \
+                            "TODO: placement on vehicle", \
+                            "TODO: mpn", \
+                            "TODO: interchange", \
+                            "TODO: surface finish", \
+                            "TODO: superseded part #", \
+                            "TODO: partslink", \
+                            "TODO: oem", \
+                            "TODO: color", \
+                            ])
+db_connection.close()

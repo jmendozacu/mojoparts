5f230d94fa60856736056fedd9850ba49053f452
Ryan Hand <ryanwhand@gmail.com>
2019-07-29 09:25:50 -0400

    move settings to config file and allow config file to be passed as command line arg

--- dev/ListingAutomation/database_config-sample.py
+++
@@ -1,6 +0,0 @@
-# RENAME THIS TO database_config.py and remove this comment
-# NOTE: make sure to add any changes to database_config-sample.py
-mysql = {'host': '## INSERT HOSTNAME HERE ##',
-         'user': '## INSERT USER HERE ##',
-         'passwd': '## INSERT PASSWORD HERE ##',
-         'db': '## INSERT DATABSE NAME HERE ##'}
